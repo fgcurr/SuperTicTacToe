db4592779622178aa644a1a1fda8464409a6bdf3
rabeet <raf6000@gmail.com>
2015-03-08 16:50:35 -0400

    Added some tests according to the SRS - Rabeet

--- src/SuperTicTacToe.java
+++ src/SuperTicTacToe.java
@@ -1,5 +1,46 @@
-// Test
+import org.junit.*;
 
 public class SuperTicTacToe {
-
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	/**
+	 * Tests if JFrame class loads successfully
+	 */
+	@Test
+	public void testJFrame() {
+		throw new RuntimeException();
+	}
+	
+	
+	/**
+	 * Test number 3
+	 * Tests if the game has ended when the number of Xs or Os have reached 40.
+	 */
+	@Test
+	public void testNumberOfCharacters() {
+		throw new RuntimeException();
+	}
+	
+	/**
+	 * Test number 5
+	 * Tests the rejection of clicks on a quadrant that has been finished
+	 */
+	@Test
+	public void testFinishedQuadrantRejectingClicks(){
+		throw new RuntimeException();
+	}
+	
+	/**
+	 * Test number 9
+	 * Tests if AI's move is indeed in a valid box.
+	 */
+	@Test
+	public void testAIsNextTurn(){
+		throw new RuntimeException();
+	}
+	
 }
