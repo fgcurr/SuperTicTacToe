1aa895ccd2a726f857007e13de82c8b16d7d8dd4
rhernandez04 <h.richard04@yahoo.com>
2015-03-13 00:39:00 -0400

    Added BoxIT.java and TicTacToeIT.java

---
+++ src/BoxIT.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class BoxIT {
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	/**
+	 * Tests if the box has a O character
+	 */
+	@Test
+	public void testIsO(){
+		throw new RuntimeException();
+	}
+	
+	/**
+	 * Tests if the box has a X character
+	 */
+	@Test
+	public void testIsX(){
+		throw new RuntimeException();
+	}
+	
+	/**
+	 * Tests if the character in the box is nothing
+	 */
+	public void testIsEmpty(){
+		throw new RuntimeException();
+	}
+}
