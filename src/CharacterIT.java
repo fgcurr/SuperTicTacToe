5a9e6d564cc5ce463c7357c3b6d175bf08ba53eb
rhernandez04 <h.richard04@yahoo.com>
2015-03-13 01:01:09 -0400

    Added quadranttIT.java and CharacterIT.java

---
+++ src/CharacterIT.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CharacterIT {
+
+	@Before
+	public void setUp() {
+		
+	}
+}
---
+++ src/quadrantIT.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class quadrantIT {
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	/**
+	 * Tests if the TicTacToe is done
+	 */
+	@Test
+	public void testTicTacToeDone(){
+		throw new RuntimeException();
+	}
+	
+	/**
+	 * Tests who was the victor of a quadrant
+	 */
+	@Test
+	public void testWhoWonQuadrant(){
+		throw new RuntimeException();
+	}
+}
