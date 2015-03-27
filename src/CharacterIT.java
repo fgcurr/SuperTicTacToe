3e15ab70926a5a57fb950edddd1856ea2b40d7fb
rhernandez04 <h.richard04@yahoo.com>
2015-03-26 23:55:46 -0400

    Added logic to CharacterIT.java

--- src/CharacterIT.java
+++ src/CharacterIT.java
@@ -1,19 +1,24 @@
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class CharacterIT {
 
+	boardElements.Character chara;
+	
 	@Before
 	public void setUp() {
-		
+		chara = new boardElements.Character();
+		chara.type ="X";
 	}
 	
 	/**
-	 * Tests if the character is of the type char.
+	 * Tests if the character is of the type String.
 	 */
 	@Test
-	public void testIsChar(){
-		throw new RuntimeException();
+	public void testIsString(){
+		assertTrue("Not of type string.", chara.type.getClass() == String.class);
 	}
 }
