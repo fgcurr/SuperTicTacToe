313021e7617cfd8a7715c6c6c8c5557389a27861
rhernandez04 <h.richard04@yahoo.com>
2015-04-20 21:00:10 -0400

    Restart and exit button now work.

--- src/main/java/game/superTicTacToe/Move.java
+++ src/main/java/game/superTicTacToe/Move.java
@@ -1,6 +1,10 @@
 package main.java.game.superTicTacToe;
 
+import java.util.Random;
+
 public class Move {
+	
+	int firstMove;
 	
 	public Move() {
 		
@@ -10,5 +14,20 @@ public class Move {
 		b.set(character);
 	}
 	
-	
+	/**
+	 * Picks who will take the first move.
+	 * 
+	 * @return
+	 * 0 - The AI will take the first move.<br>
+	 * 1 - The Human will take the first move.
+	 */
+	public int pickFirstMove(){
+		Random rndm = new Random();
+		
+		firstMove = rndm.nextInt(2);
+		
+		System.out.println(firstMove);
+		
+		return firstMove;
+	}
 }
